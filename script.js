2d9b52a1567e2ca99ba84032b487c132a9a26a3c
Mike Sasiela <m5455y@gmail.com>
2020-12-17 22:04:01 -0600

    Starting to work on user inputed search

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+$(document).ready(function () {
+//------------Current Stats------------------
 var covidURL = 'https://api.covidtracking.com/v1/us/current.json'
 $.ajax({
   url: covidURL,
   method: "GET"
 }).then(function(response){
+  var fixedDate = moment(response[0].lastModified).format("M/D/YYYY")
   var dailyUp = $('<h6>').text('Daily Death Increase: ' + response[0].deathIncrease)
   var recovered = $('<h6>').text('Total Recovered Cases: ' + response[0].recovered)
   var positive = $('<h6>').text('Total Positives: ' + response[0].positive)
-  var lastMod = $('<h6>').text('Last updated: ' + response[0].lastModified)
+  var lastMod = $('<h6>').text('Last updated: ' + fixedDate)
   $('#tracker').append(dailyUp, recovered, positive, lastMod)
 })
 
+//-------------Search States----------------------------
+function userSearch (){
+var stateURL = 'https://api.covidtracking.com/v1/states/' + stateSearch + '/info.json'
+var stateSearch = $().attr("")
+
+  $.ajax({
+    url: stateURL,
+    method: "GET"
+  }).then(function(response){
+    console.log(response)
+
+
+  })
+}
+   
+
+
+//___________________________JOKES______________________________________________
 //Blonde Joke
 function blondeJoke() {
   // Sets Variables for blonde joke API
@@ -28,13 +49,10 @@ function blondeJoke() {
   })
 }
 
-// Event Listeners
-$('#blonde-button').on('click', function () {
-  blondeJoke()
-})
+
 
 //JOD Joke
-function blondeJoke() {
+function jodJoke() {
   // Sets Variables for jod joke API
   var queryURL = "https://api.jokes.one/jod?category=jod&apiid=77bb87454dmsh74b81baa5edac7cp174c22jsn897e53317c87"
 
@@ -45,21 +63,17 @@ function blondeJoke() {
   }).then(function (response) {
     console.log(response)
 
-    $('.blonde-title').text(response.contents.jokes[0].joke.title)
-    $('.blonde-content').text(response.contents.jokes[0].joke.text)
+    $('.jod-title').text(response.contents.jokes[0].joke.title)
+    $('.jod-content').text(response.contents.jokes[0].joke.text)
 
   })
 }
 
-// Event Listeners
-$('#blonde-button').on('click', function () {
-  blondeJoke()
-})
 
 //Animal Joke
-function blondeJoke() {
+function animalJoke() {
   // Sets Variables for animal joke API
-  var queryURL = "https://api.jokes.one/jod?category=blonde&apiid=77bb87454dmsh74b81baa5edac7cp174c22jsn897e53317c87"
+  var queryURL = "https://api.jokes.one/jod?category=animal&apiid=77bb87454dmsh74b81baa5edac7cp174c22jsn897e53317c87"
 
   // Calls animal joke API
   $.ajax({
@@ -68,21 +82,17 @@ function blondeJoke() {
   }).then(function (response) {
     console.log(response)
 
-    $('.blonde-title').text(response.contents.jokes[0].joke.title)
-    $('.blonde-content').text(response.contents.jokes[0].joke.text)
+    $('.animal-title').text(response.contents.jokes[0].joke.title)
+    $('.animal-content').text(response.contents.jokes[0].joke.text)
 
   })
 }
 
-// Event Listeners
-$('#blonde-button').on('click', function () {
-  blondeJoke()
-})
 
 //Knock Knock Joke
-function blondeJoke() {
+function knockJoke() {
   // Sets Variables for knock knock joke API
-  var queryURL = "https://api.jokes.one/jod?category=blonde&apiid=77bb87454dmsh74b81baa5edac7cp174c22jsn897e53317c87"
+  var queryURL = "https://api.jokes.one/jod?category=knock-knock&apiid=77bb87454dmsh74b81baa5edac7cp174c22jsn897e53317c87"
 
   // Calls knock knock joke API
   $.ajax({
@@ -91,19 +101,35 @@ function blondeJoke() {
   }).then(function (response) {
     console.log(response)
 
-    $('.blonde-title').text(response.contents.jokes[0].joke.title)
-    $('.blonde-content').text(response.contents.jokes[0].joke.text)
+    $('.knock-title').text(response.contents.jokes[0].joke.title)
+    $('.knock-content').text(response.contents.jokes[0].joke.text)
 
   })
 }
 
+
 // Event Listeners
-$('#blonde-button').on('click', function () {
-  blondeJoke()
-})
+  // Joke Buttons
+  $('#knock-button').on('click', function () {
+    knockJoke()
+  })
+  $('#blonde-button').on('click', function () {
+    blondeJoke()
+  })
+  $('#animal-button').on('click', function () {
+    animalJoke()
+  })
+  $('#jod-button').on('click', function () {
+    jodJoke()
+  })
+
+  // Search Button
+  $('#searchbtn').on('click', function(){
+
+  })
+
+// Modal Listener
+$('.modal').modal();
 
 
-
-$(document).ready(function () {
-  $('.modal').modal();
 });
