ba4b3fdb5c33f1d5979be3911a88534a63fc160e
Mike Sasiela <m5455y@gmail.com>
2020-12-19 11:26:40 -0600

    Main app woriking. Working on local storage now

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+var stateList = []
+
 //------------Current Stats------------------
 var covidURL = 'https://api.covidtracking.com/v1/us/current.json'
 $.ajax({
@@ -14,18 +17,44 @@ $.ajax({
 })
 
 //-------------Search States----------------------------
-function userSearch (){
-var stateURL = 'https://api.covidtracking.com/v1/states/' + stateSearch + '/info.json'
-var stateSearch = $().attr("")
+function userSearch(input) {
+var stateCurrentURL = 'https://api.covidtracking.com/v1/states/' + input + '/current.json'
 
   $.ajax({
-    url: stateURL,
+    url: stateCurrentURL,
     method: "GET"
   }).then(function(response){
     console.log(response)
 
+    // Adding Content to HTML
+    $('#data-user-card').html(`<p>Date Checked: ${moment(response.dateChecked).format("M/D/YYYY")}</p>`)
+    $('#data-user-card').append(`<p>Currently Hospitalized: ${response.hospitalizedCurrently}</p>`)
+    $('#data-user-card').append(`<p>Positive Cases: ${response.positive}</p>`)
+    $('#data-user-card').append(`<p>Positive Increases: ${response.positiveIncrease}</p>`)
+    $('#data-user-card').append(`<p>Total Negatives: ${response.negative}</p>`)
+    $('#data-user-card').append(`<p>Negative Increase: ${response.negativeIncrease}</p>`)
+    $('#data-user-card').append(`<p>Total Deaths: ${response.death}</p>`)
+    $('#data-user-card').append(`<p>Death Increase: ${response.deathIncrease}</p>`)
+    $('#data-user-card').append(`<p>Total: ${response.total}</p>`)
 
   })
+
+  var stateInfoURL = 'https://api.covidtracking.com/v1/states/' + input + '/info.json'
+
+  $.ajax({
+    url: stateInfoURL,
+    method: "GET"
+  }).then(function(response){
+    console.log(response)
+
+    // Adding Content to HTML
+    $('.title-user-card').text(response.name)
+    $('#state-webpage').attr('href', response.covid19Site)
+    $('#twitter').attr('href', 'https://twitter.com/' + response.twitter)
+
+  })
+
+
 }
    
 
@@ -124,7 +153,21 @@ function knockJoke() {
   })
 
   // Search Button
-  $('#searchbtn').on('click', function(){
+  $('#searchbtn').on('click', function(event){
+    // Preventing the button from trying to submit the form
+    event.preventDefault();
+
+    // Variable to store user input
+    var userInput = $('#search').val().trim()
+    var stateButton = $('<button>').addClass("btn waves-effect waves-light list-group-item")
+    var addButton = stateButton.text(userInput)
+    $('.list-group').append(addButton)
+    
+
+    $('#user-search-card').removeClass('hide')
+
+
+    userSearch(userInput)
 
   })
 
